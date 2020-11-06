cce95e4ae960816554a9741c4ba80c21f0daa101
Riidux <riidux@gmail.com>
2020-11-05 21:07:43 -0500

    Completed Create Movie

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,6 @@
 class Movie < ApplicationRecord
+	has_many :appearances
+	has_many :actors, through: :appearances
+	validates :first_name, :last_name, presence: true
+	validates :title, :duration, :rating, :release_date, presence:true
 end
